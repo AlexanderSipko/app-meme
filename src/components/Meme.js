5b1d01de31c5f0c609600d4cb0c0e313c39ffec1
Asipko <avsipko@gmail.com>
2022-12-04 21:36:35 +0500

    data

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,8 +2,15 @@
 import '../style/Meme.css';
 
 import React from "react"
+import {meData} from "../data/memesData.js"
+
+
 
 export default function Meme() {
+    function handleclick() {
+        console.log(meData.data.memes)
+    }
+
     return (
       <main>
       <form className="form">
@@ -17,12 +24,17 @@ export default function Meme() {
               placeholder="Bottom text"
               className="form--inputs"
           />
-          <button 
+          <button onClick={handleclick}
               className="form--button"
           >
               Get a new meme image 🖼
           </button>
       </form>
+      <button onClick={handleclick}
+              className="form--button"
+          >
+              Get a new meme image 🖼
+          </button>
     </main>
     )
 }
