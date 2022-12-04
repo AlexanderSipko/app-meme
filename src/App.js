94ef6a1fe5491b6f62faf56190fabc84a8b46da4
Asipko <avsipko@gmail.com>
2022-12-04 20:26:57 +0500

    style

--- src/App.js
+++ src/App.js
@@ -6,10 +6,9 @@ import Header from './components/Header';
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
-      <Header />
+        <Header />
     </div>
-  );
-}
+  )
+} 
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,16 @@
 import '../style/Header.css';
 
 function Headre() {
+
+  let img = require('../images/troll-face.png')
   return (
-      <h1>Headre</h1>
+    <header className="header">
+      <img src={img} alt="m"
+          className="header--image"
+      />
+      <h2 className="header--title">Meme Generator</h2>
+      <h4 className="header--project">React Course - Project 3</h4>
+  </header>
   );
 }
 
