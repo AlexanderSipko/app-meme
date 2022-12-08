4ed38a0e3d997700c16141f1e33956990c27dc05
Asipko <avsipko@gmail.com>
2022-12-08 11:13:36 +0500

    minus plus

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@
 import './style/App.css';
 import Header from './components/Header';
 import Meme from './components/Meme';
-import Plus from './components/Plus';
+// import Plus from './components/Plus';
 
 function App() {
   return (
     <div>
         <Header />
         <Meme />
-        <Plus />
+        {/* <Plus /> */}
     </div>
   )
 } 
